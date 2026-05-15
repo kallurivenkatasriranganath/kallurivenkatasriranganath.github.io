0451267d9850bdc1c4138bbf60863378bf72f14c
kallurivenkatasriranganath <kallurivenkatasriranganath@gmail.com>
2026-05-15 06:07:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const toggle = document.querySelector('.menu-toggle');
 const links = document.querySelector('.nav-links');
 toggle.addEventListener('click', () => links.classList.toggle('open'));
 links.querySelectorAll('a').forEach(link => link.addEventListener('click', () => links.classList.remove('open')));
+
+
+const closeBanner =
+  document.getElementById('close-banner');
+
+const birthdayBanner =
+  document.getElementById('birthday-banner');
+
+if (closeBanner && birthdayBanner) {
+
+  closeBanner.addEventListener('click', () => {
+
+    birthdayBanner.style.display = 'none';
+  });
+}
