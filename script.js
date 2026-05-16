238c3455d1b6f5d3909073f64eb6f1f363842cfa
kallurivenkatasriranganath <kallurivenkatasriranganath@gmail.com>
2026-05-17 00:05:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,3 @@ const toggle = document.querySelector('.menu-toggle');
 const links = document.querySelector('.nav-links');
 toggle.addEventListener('click', () => links.classList.toggle('open'));
 links.querySelectorAll('a').forEach(link => link.addEventListener('click', () => links.classList.remove('open')));
-
-
-/* =========================
-   Birthday Banner
-========================= */
-
-const closeBanner =
-  document.getElementById('close-banner');
-
-const birthdayBanner =
-  document.getElementById('birthday-banner');
-
-if (closeBanner && birthdayBanner) {
-
-  closeBanner.addEventListener('click', () => {
-
-    birthdayBanner.style.display = 'none';
-  });
-}
-setTimeout(() => {
-  birthdayBanner.style.display = 'none';
-}, 8000);
